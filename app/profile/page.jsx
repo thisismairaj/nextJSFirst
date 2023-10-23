57ffff56dd93e216e10343bf1b1121f00a284045
Mairaj Pirzada <thisismairaj@gmail.com>
2023-10-23 13:19:09 +0500

    Update profile/page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,7 +7,7 @@ import Profile from "@components/profile"
 
 const MyProfile = () => {
   const { data: session } = useSession()
-  const [posts, setPosts] = useState([])
+  const [myPosts, setMyPosts] = useState([])
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,14 +17,33 @@ const MyProfile = () => {
       setPosts(data)
     }
     if (session?.user.id) fetchPosts()
-  }, [])
-  const handleEdit = (post) => {}
-  const handleDelete = async (post) => {}
+  }, [session?.user.id])
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        })
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id)
+        setMyPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
   return (
     <Profile
       name="My profile"
       desc="Welcome to profile"
-      data={[]}
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
